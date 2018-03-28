1262a86b7e6361032ae085244b00da69351d4354
David Zhang <cajan2@163.com>
2018-03-29 00:40:54 +0800

    modify transaction

--- main.go
+++ main.go
@@ -6,29 +6,7 @@ import (
 )
 
 func main() {
-	//testBlockchain()
 	cliRun()
-}
-func testBlockchain() {
-	bc := NewBlockchain()
-	defer bc.db.Close()
-	bci := bc.Iterator()
-	bc.AddBlock("Send 1 BTC to Ivan")
-	bc.AddBlock("Send 2 more BTC to Ivan")
-	for {
-		block := bci.Next()
-
-		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		pow := NewProofOfWork(block)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-
-		if len(block.PrevBlockHash) == 0 {
-			break
-		}
-	}
 }
 
 func cliRun() {
--- proofofwork.go
+++ proofofwork.go
@@ -31,7 +31,7 @@ func (pow *ProofOfWork) prepareData(nonce int) []byte {
 	data := bytes.Join(
 		[][]byte{
 			pow.block.PrevBlockHash,
-			pow.block.Transactions(),
+			pow.block.HashTransactions(),
 			IntToHex(pow.block.Timestamp),
 			IntToHex(int64(targetBits)),
 			IntToHex(int64(nonce)),
