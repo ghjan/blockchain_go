eb0decd1cd71d2db1f7c343602589aeeb45d1194
jian zhang <jian.zhang@fuwo.com>
2018-04-10 09:40:35 +0800

    add delete_blockchain.sh

--- create_blockchain.sh
+++ create_blockchain.sh
@@ -2,9 +2,15 @@
 export NODE_ID=3000
 check_results=$(blockchain_go createwallet)
 echo $check_results
-result=$(blockchain_go createblockchain -address "$check_results")
-chain_result=$(echo ${result% *})
-chain_result=$(echo ${chain_result##* })
-chain_result=$(echo ${chain_result##*\r})
+blockchain_go createblockchain -address "$check_results" > temp.txt
+sed 's/Done!//g' temp.txt >temp2.txt
+sed 's/ //g' temp2.txt >temp3.txt
+sed 's/\r/,/g' temp3.txt >temp4.txt
+
+chain_result=${result//Done/}
+chain_result=${chain_result// /}
+chain_result=${result//\\r/,}
+chain_result=${arr[${#chain_result[@]}-1]}
 export CENTREAL_NODE=$chain_result
 cp blockchain_3000.db blockchain_genesis.db
+tail
