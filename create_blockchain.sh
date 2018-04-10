423424e610276cca55612a5daa5b0f6da0655027
jian zhang <jian.zhang@fuwo.com>
2018-04-10 10:47:08 +0800

    modify create_blockchain.sh

--- create_blockchain.sh
+++ create_blockchain.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 export NODE_ID=3000
 check_results=$(blockchain_go createwallet)
-check_results=${check_results//Your new address: /}
-blockchain_go createblockchain -address "$check_results" > temp.log
+CENTRAL_NODE=${check_results//Your new address: /}
+blockchain_go createblockchain -address "$CENTRAL_NODE"
+:<<!
+> temp.log
 sed -e 's/\r/\n/g;s/Done!//g;' temp.log >temp2.log
 tail -n 3 temp2.log >temp3.log
 chain_result=$(cat temp3.log |sed '/^$/d')
-export CENTRAL_NODE=$chain_result
+!
 cp blockchain_3000.db blockchain_genesis.db
