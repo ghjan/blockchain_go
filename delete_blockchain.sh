85cbd81b4b4167a12106516e86eb4ff0987023fd
jian zhang <jian.zhang@fuwo.com>
2018-04-12 13:08:21 +0800

    save shell

--- delete_blockchain.sh
+++ delete_blockchain.sh
@@ -2,3 +2,15 @@
 rm -rf *.db
 rm -rf *.dat
 rm -rf *.log
+
+wallets=env |grep WALL |awk 'BEGIN {FS="="} {print $1}'
+for w in $wallets
+    do
+        unset $w
+    done
+
+nodes=env |grep NODE |awk 'BEGIN {FS="="} {print $1}'
+for node in $nodes
+    do
+        unset $node
+    done
