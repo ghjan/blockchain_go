0d283e05c60fb3e258361a96f577e4f8d73c31dd
jian zhang <jian.zhang@fuwo.com>
2018-04-12 11:21:38 +0800

    modify shell

--- listbalance_wallet.sh
+++ listbalance_wallet.sh
@@ -18,8 +18,24 @@ fi
 if [ -z "$MINER_WALLET" ]; then
     export MINER_WALLET=$(cat wallet4.log)
 fi
-blockchain_go getbalance -address $CENTRAL_NODE
-blockchain_go getbalance -address $WALLET_1
-blockchain_go getbalance -address $WALLET_2
-blockchain_go getbalance -address $WALLET_3
-blockchain_go getbalance -address $MINER_WALLET
+
+if [ -n "$CENTRAL_NODE" ]; then
+    echo "balance for CENTRAL_NODE"
+    blockchain_go getbalance -address $CENTRAL_NODE
+fi
+if [ -n "$WALLET_1" ]; then
+    echo "balance for WALLET_1"
+    blockchain_go getbalance -address $WALLET_1
+fi
+if [ -n "$WALLET_2" ]; then
+    echo "balance for WALLET_2"
+    blockchain_go getbalance -address $WALLET_2
+fi
+if [ -n "$WALLET_3" ]; then
+    echo "balance for WALLET_3"
+    blockchain_go getbalance -address $WALLET_3
+fi
+if [ -n "$MINER_WALLET" ]; then
+    echo "balance for MINER_WALLET"
+    blockchain_go getbalance -address $MINER_WALLET
+fi
